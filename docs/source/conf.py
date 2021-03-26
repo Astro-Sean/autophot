cd6c34cbea8992e7524749536545cd58f19bdcc7
Seán Brennan <brennan4661@hotmail.com>
2021-03-26 17:13:32 +0000

    initial documentation commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,13 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_logo = '../../logo.png'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
